8f8e32f48436e5e081f2db21b343f38bfee427eb
lee-hees <2sdsf00@naver.com>
2020-04-10 18:26:40 +0900

    number

---
+++ PLT_Project/PLT_Project/number.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+int main(){
+
+	char i;
+	i = 127; // 10진수
+	printf("10진수 : %i\r\n", i);
+	printf("16진수 : 0x%x\r\n", i);
+	printf("08진수 : %o\r\n", i);
+	printf("문자형 : %c\r\n", i);
+
+	puts("");
+
+	i = 0x7f; // 16진수 // '0x'를 붙여 16진수라고 표시해줌
+	printf("10진수 : %i\r\n", i);
+	printf("16진수 : 0x%x\r\n", i);
+	printf("08진수 : %o\r\n", i);
+	printf("문자형 : %c\r\n", i);
+	return 0;
+
+}
