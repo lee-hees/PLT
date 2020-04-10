97f4c28cab914f1d8af51df15615c303c4be1cea
lee-hees <2sdsf00@naver.com>
2020-04-10 19:45:47 +0900

    pointer

---
+++ PLT_Project/PLT_Project/pointer.cpp
@@ -0,0 +1,49 @@
+﻿#include <stdio.h>
+
+void main(){
+
+	printf("-----pointer-----\r\n");
+
+	int i;
+	int j = 2;
+	i = j; // assignment operator(대입 연산자)
+	int k = 3;
+	j = k;
+
+	int* ip;
+	ip = &j; // & : address of operator(주소 연산자)
+
+	printf("  i - %i\r\n", i);
+	printf(" &j - %p\r\n", &j);
+	printf(" ip - %p\r\n", ip);
+	// dereference operator(contents of operator)(역참조 연산자(실제 값))
+	printf("  j - %i\r\n", j);
+	printf("*ip - %i\r\n", *ip);
+	puts("");
+
+	printf("-----string literal-----\r\n");
+
+	const char* s;
+	s = "hello";
+
+	int si = 0;
+	int sj = 0;
+
+	si = *s; // 'h'의 아스키코드
+	sj = *(s + 1); // 'e'의 아스키코드
+
+	printf("%p\r\n", s); // 주소
+	printf("%c\r\n", *s); // 문자
+	printf("%s\r\n", s); // 문자열
+	printf("%i\r\n", *s); // 아스키코드
+	puts("");
+	printf("%i\r\n", si); // 'h'의 아스키코드
+	printf("%i\r\n", sj); // 'e'의 아스키코드
+	puts("");
+	printf("%%i --> %3i / %%c --> %c\r\n", *(s + 1), *(s + 1)); // 아스키코드 / 문자
+	printf("%%i --> %3i / %%c --> %c\r\n", s[1], s[1]); // 아스키코드 / 문자
+	printf("%%i --> %3i / %%c --> %c\r\n", 1[s], 1[s]); // 아스키코드 / 문자
+	printf("%%i --> %3i / %%c --> %c\r\n", s[5], s[5]); // 아스키코드 / 문자
+
+	return;
+}
