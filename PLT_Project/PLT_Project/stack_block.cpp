bcdd991b87ae9ea07ee78d11b4f897ead2af8525
lee-hees <2sdsf00@naver.com>
2020-04-30 20:09:53 +0900

    stack block

---
+++ PLT_Project/PLT_Project/stack_block.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+void main()
+{
+	int i = 2;
+	int j = 3;
+	char* ip = (char*)&i;
+	char* jp = (char*)&j;
+
+	printf("%i\r\n", &i - &j);
+	printf("%i\r\n", ip - jp);
+
+	return;
+}
