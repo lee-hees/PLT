8f05a20ee4b204eaf820c83eecac318ee823abd5
lee-hees <2sdsf00@naver.com>
2020-04-30 20:04:33 +0900

    (&) - address of operator

---
+++ PLT_Project/PLT_Project/address_of_operator.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+
+void Swap(int* i, int* j){
+	int t = *i;
+	*i = *j;
+	*j = t;
+}
+
+void main()
+{
+	int i = 2;
+	int j = 3;
+	int t;
+	printf("%i, %i\r\n", i, j);
+
+	Swap(&i, &j);
+
+	printf("%i, %i\r\n", i, j);
+
+	return;
+}
