2fc432b22e5cbd7a6182cabbb86393e8db037e3f
lee-hees <2sdsf00@naver.com>
2020-05-09 22:50:30 +0900

    switch...case

---
+++ PLT_Project/PLT_Project/switch_case.cpp
@@ -0,0 +1,40 @@
+﻿#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	char ch;
+
+	ch = _getch();
+	//if (ch == 97)
+	if (ch == 'a' || ch == 'A')
+	{
+		printf("abc\r\n");
+	}
+	//else if (ch == 104)
+	else if (ch == 'h' || ch == 'H')
+	{
+		printf("hello\r\n");
+	}
+	else
+	{
+		printf("nothing\r\n");
+	}
+
+	ch = _getch();
+	switch (ch){
+	case 'a':
+	case 'A':
+		printf("abc\r\n");
+		break; // 미작성 시 아래의 동작도 수행됨
+	case 'h':
+		//case 'a' + 7:
+	case 'H':
+		printf("hello\r\n");
+		break; // 미작성 시 아래의 동작도 수행됨
+	default:
+		printf("nothing\r\n");
+	}
+
+	return 0;
+}
